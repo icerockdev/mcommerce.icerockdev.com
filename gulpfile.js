50ce2a2bc37ad954341ad5699fc96c959d2d4ec7
tamat <libastral.so@yandex.ru>
2021-06-08 18:00:06 +0700

    Cleanup code

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-const gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	del = require('del'),
-	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache'),
-	autoprefixer = require('gulp-autoprefixer'),
-	bourbon = require('node-bourbon'),
-	notify = require("gulp-notify");
+/* Copyright (c) 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license. */
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const autoprefixer = require('gulp-autoprefixer');
+const bourbon = require('node-bourbon');
+const notify = require("gulp-notify");
 
 // Скрипты проекта
 gulp.task('scripts', function () {
@@ -64,26 +66,24 @@ gulp.task('imagemin', function () {
 });
 
 gulp.task('build', ['removedist', 'imagemin', 'sass', 'scripts'], function () {
-
-  var buildFiles = gulp.src([
+  gulp.src([
     'app/*.html',
     'app/.htaccess',
     'app/*.json',
     'app/*.php',
   ]).pipe(gulp.dest('dist'));
 
-  var buildCss = gulp.src([
+  gulp.src([
     'app/css/main.min.css',
   ]).pipe(gulp.dest('dist/css'));
 
-  var buildJs = gulp.src([
+  gulp.src([
     'app/js/scripts.min.js',
   ]).pipe(gulp.dest('dist/js'));
 
-  var buildFonts = gulp.src([
+  gulp.src([
     'app/fonts/**/*',
   ]).pipe(gulp.dest('dist/fonts'));
-
 });
 
 
